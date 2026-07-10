dea67275688aa6e6df16be21196b2e478a6b3501
Ahed11 <ahdghandi73@gmail.com>
2026-07-10 03:08:03 +0300

    доработка функций приема данных

--- cmd/policy-playground/main.go
+++ cmd/policy-playground/main.go
@@ -13,6 +13,24 @@ type RunConfig struct {
 	OutPath	string
 }
 
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: policy-playground <command> [options]")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "run":
+		if err := runCmd(os.Args[2:]); err != nil {
+			fmt.Fprintln(os.Stderr, "Error:", err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+}
+
 func runCmd(args []string) error {
 	fs := flag.NewFlagSet("run", flag.ContinueOnError)
 
@@ -45,22 +63,4 @@ func run(cfg RunConfig) error {
 	//дальше надо будет прописать чтение параметров(сценария, политик) и запуск симуляции. После попытаться написать alerts.jsonl
 
 	return nil
-}
-
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage: policy-playground <command> [options]")
-		os.Exit(1)
-	}
-
-	switch os.Args[1] {
-	case "run":
-		if err := runCmd(os.Args[2:]); err != nil {
-			fmt.Fprintln(os.Stderr, "Error:", err)
-			os.Exit(1)
-		}
-	default:
-		fmt.Fprintf(os.Stderr, "unknown command: %s\n", os.Args[1])
-		os.Exit(1)
-	}
 }
