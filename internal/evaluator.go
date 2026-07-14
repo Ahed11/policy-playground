b206f891754223614950fa3a7a43b1f4ac172aa2
Ahed11 <ahdghandi73@gmail.com>
2026-07-14 16:41:09 +0300

    добавлена логика all

--- internal/evaluator.go
+++ internal/evaluator.go
@@ -4,32 +4,44 @@ import (
 	"fmt"
 )
 
-func CheckIfEquals() {
-	policies, err := ReadPoliciesYAML("../testdata/control/policies.yaml")
-	
-	if err != nil {
-		fmt.Println(err)
-		return 
+func CheckIfEquals(event Event, condition Condition) (result bool, reason string, err error) {
+	if condition.Field == "" {
+		return false, "", fmt.Errorf("поле пусто")
+	}
+	if condition.Field != "action" && condition.Field != "destination_type" {
+		return false, "", fmt.Errorf("поле не поддерживается")
+	}
+	switch condition.Field {
+	case "action":
+		if condition.Equals == event.Action {
+			return true, fmt.Sprintf("action equals %s", event.Action), nil
+		}
+	case "destination_type":
+		if condition.Equals == event.DestinationType {
+			return true, fmt.Sprintf("destination_type equals %s", event.DestinationType), nil
+		}
+	}
+	return false, "", nil
+}
+
+func AllConditions(event Event, condition Condition) (result bool, reasons []string, err error) {
+
+	for i := range condition.All {
+		result, reason, err := CheckIfEquals(event, condition.All[i])
+		if err != nil {
+			return result, reasons, fmt.Errorf("Ошибка: %s", err)
+		} 
+		if result != true {
+			if reasons != nil {
+				reasons = nil
+				return result, reasons, err
+			} else {
+				return result, reasons, err
+			}
+		} else {
+			reasons = append(reasons, reason)
+		}
 	}
 
-	allPolicies := policies.Policies
-	
-	conditions := []struct{}
-
-	for i, v := range allPolicies {
-		conditions = append(conditions, allPolicies[i].Condition.All)
-	}
-	
-	scenario, err := ReadScenarioYAML("../testdata/control/scenario.yaml")
-	
-	if err != nil {
-		fmt.Println(err)
-		return 
-	}
-
-	events := scenario.Events
-	
-	// for i, p := range events {
-	// 	if 
-	// }
+	return true, reasons, nil
 }
