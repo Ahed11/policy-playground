592bfe9a31ae27f300ade5be160b32d3a8467c5c
Ahed11 <ahdghandi73@gmail.com>
2026-07-18 09:04:48 +0300

    переработка AllConditions

--- internal/evaluator.go
+++ internal/evaluator.go
@@ -27,7 +27,7 @@ func CheckIfEquals(event Event, condition Condition) (result bool, reason string
 	return false, "", nil
 }
 
-func CheckifContains(event Event, condition Condition) (result bool, reason string, err error) {
+func CheckIfContains(event Event, condition Condition) (result bool, reason string, err error) {
 	if condition.Field == "" {
 		return false, "", fmt.Errorf("поле пусто")
 	}
@@ -46,22 +46,34 @@ func CheckifContains(event Event, condition Condition) (result bool, reason stri
 }
 
 func AllConditions(event Event, condition Condition) (result bool, reasons []string, err error) {
-
 	for i := range condition.All {
-		result, reason, err := CheckIfEquals(event, condition.All[i])
-		if err != nil {
-			return result, reasons, fmt.Errorf("Ошибка: %s", err)
-		} 
-		if result != true {
-			if reasons != nil {
-				reasons = nil
-				return result, reasons, err
-			} else {
-				return result, reasons, err
-			}
-		} else {
-			reasons = append(reasons, reason)
+		cond := condition.All[i]
+
+		var ok bool
+		var reason string
+		var checkErr error
+
+		switch {
+		case cond.Field != "":
+			return false, nil, fmt.Errorf("Поле не заполнено") 
+		case cond.Equals != "" && cond.Contains != "":
+			return false, nil, fmt.Errorf("Операторы equals и contains заполнены одновременно")
+		case cond.Equals == "" && cond.Contains == "":
+			return false, nil, fmt.Errorf("Операторы equals и contains не заполнены")
+		case cond.Equals != "":
+			ok, reason, checkErr = CheckIfEquals(event, cond)
+		case cond.Contains != "":
+			ok, reason, checkErr = CheckIfContains(event, cond)
 		}
+
+		if checkErr != nil {
+			return false, nil, fmt.Errorf("Ошибка %v: %w", i, checkErr)
+		}
+		if ok != true {
+			return false, nil, nil
+		}
+
+		reasons = append(reasons, reason)
 	}
 
 	return true, reasons, nil
