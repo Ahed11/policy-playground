3f015c27d137429176880bc3c4f7655db4b782f4
Ahed11 <ahdghandi73@gmail.com>
2026-07-22 02:07:48 +0300

    поменял тип колонок на нужынй (указатель)

--- internal/event.go
+++ internal/event.go
@@ -7,9 +7,9 @@ type Event struct {
 	Action string `yaml:"action"`
 	ObjectType string `yaml:"object_type"`
 	FileName *string `yaml:"file_name"`
-	FileExt string `yaml:"file_ext"`
-	ContentClasses []string `yaml:"content_classes"`
+	FileExt *string `yaml:"file_ext"`
+	ContentClasses *[]string `yaml:"content_classes"`
 	Channel string `yaml:"channel"`
 	DestinationType string `yaml:"destination_type"`
-	SizeBytes int `yaml:"size_bytes"`
+	SizeBytes *int `yaml:"size_bytes"`
 }
